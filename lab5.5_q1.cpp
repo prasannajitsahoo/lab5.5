2fe3f74b3e92d171df61cafdeb33b0d150e51e1e
prasannajitsahoo <42238651+prasannajitsahoo@users.noreply.github.com>
2018-09-07 12:23:11 +0530

    Add files via upload

--- lab5.5_q1.cpp
+++ lab5.5_q1.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 
 int main(){
-   //print n stars
+   //print stars in rows
    for(int i=0 ; i<5; i++){
+   //print stars in columns
      for (int j=0; j<5; j++){
       cout<<"*";}
+   //end line
        cout<<endl;
     }
    return 0;
